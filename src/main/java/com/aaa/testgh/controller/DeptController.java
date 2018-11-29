1b4abfd1286009f03fcd5ed736641cf42eaef762
ixiaorun <133627244@qq.com>
2018-11-29 09:31:41 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:筱润
+ * createTime:2018-11-29 09:10
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("Hello  GitHub");
+    }
+}
